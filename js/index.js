57fe85eadf6da59f5a8559ee524903a3607cc4c6
allisonwilliams92 <alwilliams@deloitte.com>
2016-04-13 19:31:06 -0400

    Added carousel, form, and comments

--- js/index.js
+++ js/index.js
@@ -1,43 +1,61 @@
 $(document).ready(function() {
+	// Listen to click for galleries title in nav menu
 	$(".galleriesMain").click(galleriesToggle);
+	// Listen to carousel button clicks on main page
 	$('.nextButton').click(nextImage);
 	$('.prevButton').click(prevImage);
+	// Set timer for carousel
 	var timer= setInterval(myTimer, 7000);
+	//Listen for hamburger click
 	$('.hamburger').click(toggleNav);
+	//Listen for click on submit button in contact form
+	$('.sendButton').click(submitForm);
+	
 
+	
+	//Set of array of images for main page carousel
 	var carousel = ['Pacific/Northwest_log','Texas/Texas_car2','Rhine/Rhine_bridge']
 
+	//Set index to keep track of current carousel picture
 	var imageIndex=0;
 
+	//Show/Hide subcategories for gallery on nav menu
 	function galleriesToggle(){
 		$(".galleries").toggle();
 	}
 
+	//Move to the next carousel image
 	function nextImage(){
 		imageIndex++;
+		//If last carousel image, return to first image
 		if(imageIndex===carousel.length){
 			imageIndex=0;
 		}
 		var imagePath="images/"+carousel[imageIndex] +'.jpg';
 		$('.imageMain').attr('src',imagePath);
+		//Restart timer
 		clearInterval(timer);
 		timer=setInterval(myTimer, 7000);
 		
 
 	}
 
+	//Move to the previous carousel image
 	function prevImage(){
 		imageIndex--;
+		//If first carousel image, move to last image
 		if(imageIndex===-1){
 			imageIndex=carousel.length-1;
 		}
 		var imagePath="images/"+carousel[imageIndex] +'.jpg';
 		$('.imageMain').attr('src',imagePath);
+		//Clear and reset timer
 		clearInterval(timer);
 		timer=setInterval(myTimer, 7000);
 
 	}
 
+	//Move to next carousel imaage after certain period of time
 	function myTimer(){
 		imageIndex++;
 		if(imageIndex===carousel.length){
@@ -50,9 +68,35 @@ $(document).ready(function() {
 
 	}
 
-
+	//Toggle showing the navigation menu
 	function toggleNav(){
 		$('.navigation').toggle();
+	}
+
+	//Validation of form content
+	function submitForm(){
+
+		//Retrieve current form content
+		var name = $('input[name="username"]').val();
+		var email = $('input[name="email"]').val();
+		
+		//Return error or success messages
+		console.log('message');
+		if(name===''){
+			alert('Please enter a name.');
+
+		}
+
+		else if(email.indexOf("@")===-1){
+			alert('Please enter a valid email address');
+
+		}
+
+		
+
+		else{
+			alert('Email sent. I will reply to you shortly!');
+		}
 	}
 
 
