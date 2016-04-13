59a7de6fc7ae21bec7dc60fa704c6a3e618e8777
allisonwilliams92 <alwilliams@deloitte.com>
2016-04-13 19:14:23 -0400

    More Updates

--- js/index.js
+++ js/index.js
@@ -1,10 +1,60 @@
 $(document).ready(function() {
 	$(".galleriesMain").click(galleriesToggle);
-	
+	$('.nextButton').click(nextImage);
+	$('.prevButton').click(prevImage);
+	var timer= setInterval(myTimer, 7000);
+	$('.hamburger').click(toggleNav);
+
+	var carousel = ['Pacific/Northwest_log','Texas/Texas_car2','Rhine/Rhine_bridge']
+
+	var imageIndex=0;
 
 	function galleriesToggle(){
 		$(".galleries").toggle();
 	}
+
+	function nextImage(){
+		imageIndex++;
+		if(imageIndex===carousel.length){
+			imageIndex=0;
+		}
+		var imagePath="images/"+carousel[imageIndex] +'.jpg';
+		$('.imageMain').attr('src',imagePath);
+		clearInterval(timer);
+		timer=setInterval(myTimer, 7000);
+		
+
+	}
+
+	function prevImage(){
+		imageIndex--;
+		if(imageIndex===-1){
+			imageIndex=carousel.length-1;
+		}
+		var imagePath="images/"+carousel[imageIndex] +'.jpg';
+		$('.imageMain').attr('src',imagePath);
+		clearInterval(timer);
+		timer=setInterval(myTimer, 7000);
+
+	}
+
+	function myTimer(){
+		imageIndex++;
+		if(imageIndex===carousel.length){
+			imageIndex=0;
+		}
+		var imagePath="images/"+carousel[imageIndex] +'.jpg';
+		$('.imageMain').attr('src',imagePath);
+		
+
+
+	}
+
+
+	function toggleNav(){
+		$('.navigation').toggle();
+	}
+
 
 
 
